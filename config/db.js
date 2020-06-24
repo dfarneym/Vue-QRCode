781c5eec0637bc2f7e03fe34b9c1807ac8d4fdf3
FarneyCdz <53848638+FarneyCdz@users.noreply.github.com>
2020-06-24 11:43:06 -0300

    Deletando

--- config/db.js
+++
@@ -1,6 +0,0 @@
-const config = require('../knexfile.js')
-const knex = require('knex') (config)
-
-//Exportando o knex para o index
-knex.migrate.latest([config])
-module.exports = knex
--- config/mongodb.js
+++
@@ -1,9 +0,0 @@
-//Conectando com o mongodb
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/seraquepresta_stats', { useNewUrlParser: true })
-    .catch(e => {
-        const msg = 'ERRO! Não foi possível conectar com o MongoDB!'
-        console.log('\x1b[41m%s\x1b[37m', msg, '\x1b[0m')
-    })
-
-   // mongodb+srv://deploy:<password>@cluster0-rgc9n.mongodb.net/test?retryWrites=true&w=majority
